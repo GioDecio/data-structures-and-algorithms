ecd0835917ad50da5aaefb01bb15da2b9951b76c
Giovanni De Cillis <giovanni.decillis@gmail.com>
2025-01-23 10:26:42 +0000

    added exercise

---
+++ ch7/find_needle.py
@@ -0,0 +1,15 @@
+def find_needle(n, h):
+    if len(h) < len(n):
+        return False
+
+    for idx in range(len(h) - len(n) + 1):
+        if n == h[idx : idx + len(n)]:
+            return True
+
+    return False
+
+
+h = input("haystack: ")
+n = input("needle: ")
+
+print(find_needle(n, h))
