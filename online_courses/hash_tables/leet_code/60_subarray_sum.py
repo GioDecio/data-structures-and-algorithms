1e57f0a4123f3c3d1b91e93b2affd7c4eb8b840e
GioDecio <giovanni.decillis@gmail.com>
2025-01-27 10:25:38 +0000

    Solved problem 60

--- online_courses/hash_tables/leet_code/60_subarray_sum.py
+++ online_courses/hash_tables/leet_code/60_subarray_sum.py
@@ -1,27 +1,41 @@
-def subarray_sum(nums, target):
+def subarray_sum_inefficient(nums, target):
 
-    hash_table = {}
     for i, n in enumerate(nums):
-        if sum(nums[i:])==target:
-            return [hash_table[i],]
+        for j, m in enumerate(nums[i:]):
+            if sum(nums[i:j+1])==target:
+                print(sum(nums[i:j+1]))
+                return [i, j]
+    return []
 
+def subarray_sum(nums, target):
+    prefix_sum = 0
+    sum_dict = {0:-1}
+    for idx, num in enumerate(nums):
+        prefix_sum +=num
+        if prefix_sum - target in sum_dict:
+            return [sum_dict[prefix_sum-target]+1, idx]
+        sum_dict[prefix_sum]=idx 
+    return []
+        
+
+    
 
 
 nums = [1, 2, 3, 4, 5]
 target = 9
 print ( subarray_sum(nums, target) )
 
-nums = [-1, 2, 3, -4, 5]
-target = 0
-print ( subarray_sum(nums, target) )
+# nums = [-1, 2, 3, -4, 5]
+# target = 0
+# print ( subarray_sum(nums, target) )
 
-nums = [2, 3, 4, 5, 6]
-target = 3
-print ( subarray_sum(nums, target) )
+# nums = [2, 3, 4, 5, 6]
+# target = 3
+# print ( subarray_sum(nums, target) )
 
-nums = []
-target = 0
-print ( subarray_sum(nums, target) )
+# nums = []
+# target = 0
+# print ( subarray_sum(nums, target) )
 
 
 
