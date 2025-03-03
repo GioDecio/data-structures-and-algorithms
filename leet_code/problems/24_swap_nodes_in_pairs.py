99323fa692e41b9a6263eabdf083d11ca8206630
GioDecio <giovanni.decillis@gmail.com>
2025-03-03 12:12:47 +0100

    solved problem 24

--- leet_code/problems/24_swap_nodes_in_pairs.py
+++ leet_code/problems/24_swap_nodes_in_pairs.py
@@ -5,13 +5,49 @@ You must solve the problem without modifying the values in the list's nodes
 """
 
 
+from functools import wraps
+
+def print_list_after(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        result = func(self, *args, **kwargs)
+        print("\nList after", func.__name__ + ":")
+        temp = self.head
+        while temp is not None:
+            print(temp.value, end=" -> ")
+            temp = temp.next
+        #print("None")
+        # print(f"Return value from {func.__name__}: {result}")  # Add this line to debug
+        return result
+    return wrapper
+
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
 
+class LinkedList:
+    @print_list_after
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+    
+    @print_list_after
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
 
-def swapPairs(self, head):
+# @print_list_after
+def swapPairs(head):
     """
     :type head: Optional[ListNode]
     :rtype: Optional[ListNode]
@@ -28,3 +64,17 @@ def swapPairs(self, head):
     second.next = first
 
     return second
+
+    
+linked_list = LinkedList(1)
+linked_list.append(5)
+linked_list.append(100)
+linked_list.append(200)
+print('\n')
+print(swapPairs(linked_list.head).value)
+print(swapPairs(linked_list.head.next).value)
+#print(swapPairs(linked_list.head).next.next.value)
+# print(swapPairs(linked_list.head).next.next.next.value)
+
+
+
