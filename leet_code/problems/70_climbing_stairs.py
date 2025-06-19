bb2d7872a29260bb21d604b16fd02326c752abae
Giovanni DeCillis <giovannidecillis@Giovannis-MacBook-Pro.local>
2025-06-19 23:56:22 +0100

    solved 70

---
+++ leet_code/problems/70_climbing_stairs.py
@@ -0,0 +1,22 @@
+def climbing_recursive(n):
+
+    if n==1:
+        return 1
+    if n==2:
+        return 2
+    return climbing_recursive(n-2) + climbing_recursive(n-1)
+
+
+def climbing_memo(n):
+    
+    memo = {1:1, 2:2}
+    
+    def f(n):
+        if n in memo:
+            return memo[n]
+        else:
+            memo[n] = f(n-2) + f(n-1)
+        return memo[n]
+
+    return f(n)
+    
